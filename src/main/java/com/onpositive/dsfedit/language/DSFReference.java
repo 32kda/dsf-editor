3925459562e2ddca4e9f5dcd89584f1b4b2046c8
Dmitry Karpenko <dmitry@onpositive.com>
2020-05-30 00:25:13 +0700

    Referencing started to work

--- src/main/java/com/onpositive/dsfedit/language/DSFReference.java
+++ src/main/java/com/onpositive/dsfedit/language/DSFReference.java
@@ -1,11 +1,8 @@
 package com.onpositive.dsfedit.language;
 
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementResolveResult;
 import com.intellij.psi.PsiReferenceBase;
-import com.intellij.psi.ResolveResult;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.onpositive.dsfedit.language.parser.psi.*;
 import org.jetbrains.annotations.NotNull;
@@ -13,13 +10,12 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class DSFReference extends PsiReferenceBase<PsiElement> {
 
     private final String key;
 
-    public DSFReference(@NotNull PsiElement element, TextRange textRange) {
+    public DSFReference(@NotNull PsiElement element, TextRange textRange, String text) {
         super(element, textRange);
         key = element.getText().substring(textRange.getStartOffset(), textRange.getEndOffset());
     }
@@ -29,12 +25,12 @@ public class DSFReference extends PsiReferenceBase<PsiElement> {
     public PsiElement resolve() {
         try {
             int index = Integer.parseInt(key);
-            if (myElement.getParent() instanceof DSFObject) {
+            if (myElement instanceof DSFObject) {
                 Collection<DSFObjectDef> children = PsiTreeUtil.findChildrenOfType(myElement.getParent(), DSFObjectDef.class);
                 if (children.size() > index) {
                     return new ArrayList<>(children).get(index);
                 }
-            } else if (myElement.getParent() instanceof DSFPolygon) {
+            } else if (myElement instanceof DSFPolygon) {
                 Collection<DSFPolygonDef> children = PsiTreeUtil.findChildrenOfType(myElement.getParent(), DSFPolygonDef.class);
                 if (children.size() > index) {
                     return new ArrayList<>(children).get(index);
--- src/main/java/com/onpositive/dsfedit/language/DSFReferenceContributor.java
+++ src/main/java/com/onpositive/dsfedit/language/DSFReferenceContributor.java
@@ -1,24 +1,30 @@
 package com.onpositive.dsfedit.language;
 
-import com.intellij.openapi.util.TextRange;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ProcessingContext;
 import com.onpositive.dsfedit.language.parser.psi.DSFIntRef;
 import org.jetbrains.annotations.NotNull;
-import org.picocontainer.defaults.SimpleReference;
 
 public class DSFReferenceContributor extends PsiReferenceContributor {
 
     @Override
     public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
-        registrar.registerReferenceProvider(PlatformPatterns.psiElement(DSFIntRef.class),
+        registrar.registerReferenceProvider(PlatformPatterns.psiElement(PsiElement.class),
             new PsiReferenceProvider() {
                 @NotNull
                 @Override
                 public PsiReference[] getReferencesByElement(@NotNull PsiElement element,
                                                              @NotNull ProcessingContext context) {
-                    return new PsiReference[]{new DSFReference(element, element.getTextRange())};
+                    if (element instanceof DSFIntRef) {
+                        return new PsiReference[]{new DSFReference(element, element.getTextRange(), element.getText())};
+                    }
+                    DSFIntRef child = PsiTreeUtil.findChildOfType(element, DSFIntRef.class);
+                    if (child != null) {
+                        return new PsiReference[]{new DSFReference(element, child.getTextRangeInParent(), child.getText())};
+                    }
+                    return new PsiReference[0];
                 }
             });
     }
